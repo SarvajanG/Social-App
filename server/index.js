9587f7f3d4948861df539c84e9eeef27bdcd6ae9
Sarvajan <sarvajan321@hotmail.com>
2025-02-23 02:20:38 -0800

    setup authentication and authorization

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import {register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +36,13 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.post("/auth", authRoutes);
+app.post("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 4001;
